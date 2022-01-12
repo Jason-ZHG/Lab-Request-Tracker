f6162b74e2e7f41deab87f638d3a18c9838ab0d6
Hanguang Zheng <hgzheng@seas.upenn.edu>
2022-01-12 16:22:11 -0500

    Transferred all files

---
+++ src/GUI.java
@@ -0,0 +1,11 @@
+/**
+ * This is an interface that will be implemented by all the GUI classes
+ * @author Jason Zheng, Alex Xia
+ *
+ */
+public interface GUI {	
+	/**
+	 * This method will initialize a GUI window
+	 */
+	public void initialize();
+}
